15ce8809309f9be9c59b06a0f7e0d1ea80536f6e
Usman Arshad <us.arshad@gmail.com>
2024-05-17 21:32:05 +0500

    Initial Commit

---
+++ SwiftBasicsBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+// Printing to the console
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+
+// Comments in Swift
+
+// This is a single line comment
+/*
+ This is a muliline
+ Comments
+ that goes to
+ multiple lines
+ */
