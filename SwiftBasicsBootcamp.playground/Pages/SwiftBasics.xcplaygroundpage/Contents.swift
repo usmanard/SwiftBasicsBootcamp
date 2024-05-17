1f7595214322559e65b7cea617ec0d4a946b43e8
Usman Arshad <us.arshad@gmail.com>
2024-05-17 23:06:21 +0500

    [Recomendation] Swift Recomended Variable Naming Convention

--- SwiftBasicsBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
+++ SwiftBasicsBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -23,4 +23,18 @@ print(greeting)
 
 // Constant Variable using let
 
+// Camel Case - CORRECT
+// The first word is lowercased and then the first character in every following word is uppercased.
+
 let firstGreeting = "Hello World!"
+
+// Snake Case
+let this_is_what_snake_case = "Hello World"
+
+// Pascal Case
+let ThisIsWhatPascalCase = "Hello World"
+
+// Camel Case
+let thisIsWhatCamelCase = "Hello World"
+
+// Convention used in Swift always recomended called Camel Case
