6db16c1ac6cdb4512c0922f860697b08cd806178
Usman Arshad <us.arshad@gmail.com>
2024-05-17 22:59:58 +0500

    Declaring variable using let for the constant value

--- SwiftBasicsBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
+++ SwiftBasicsBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -11,9 +11,16 @@ print(greeting)
 // Comments in Swift
 
 // This is a single line comment
+
 /*
  This is a muliline
  Comments
  that goes to
  multiple lines
  */
+
+// Naming Conventions
+
+// Constant Variable using let
+
+let firstGreeting = "Hello World!"
